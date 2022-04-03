6ceac74867cc18ffa741c4244e8861cc6762afa0
Gauthier Chaty <gauthier.chaty@gmail.com>
2022-04-03 22:43:49 +0200

    Add Theatre class

--- livrel/Theatre.py
+++ livrel/Theatre.py
@@ -1,18 +1,29 @@
 import io
+import json
 import re
 from collections import defaultdict
 
 
 class Theatre:
+
     def __init__(self, filename, act, encoding='utf8'):
         self.filename = filename
         self.act = act
+
+        self.verse_id = -1
+        self._temp_dict = defaultdict(dict)
+        self.scene_dict = defaultdict(int)
+        self.act_dict = defaultdict(dict)
 
         self.data = self.load_txt(encoding)
         self.lines = self.get_lines()
         self.nb_scene = self.get_nb_scene()
         self.characters = self.get_act_characters()
         self.scene_lines = self.get_scene_lines()
+
+        self.analyse_act()
+        self.save_json('test.json')
+
 
     def load_txt(self, encoding):
         with io.open(self.filename, 'r', encoding=encoding) as infile:
@@ -70,3 +81,80 @@ class Theatre:
                     stage_direction = match.group(2)
 
         return charac, stage_direction
+
+    def scene_lines_to_struct(self, scene):
+
+        prec_character = ''
+        index_dialogue = 0
+        verses = []
+        for index in range(2, len(self.scene_lines[scene])):
+            line = self.scene_lines[scene][index]
+            character, stage_direction = self.get_character(line)
+            #print(index, len(self.scene_lines[scene]), line, character, stage_direction)
+
+            if character is not None:
+                if not prec_character == '':
+                    verses_dict = self.verses_to_struct(verses)
+                    self._temp_dict[index_dialogue] = dict(character=prec_character, verses=verses_dict)
+
+                    if stage_direction is not None:
+                        self._temp_dict[index_dialogue]['stage_direction'] = stage_direction
+
+                    index_dialogue += 1
+
+                prec_character = character
+                verses = []
+            elif index == len(self.scene_lines[scene])-1:
+                verses.append(line)
+                verses_dict = self.verses_to_struct(verses)
+                self._temp_dict[index_dialogue] = dict(character=prec_character, verses=verses_dict)
+            else:
+                verses.append(line)
+
+        return self._temp_dict
+
+    def verses_to_struct(self, verses):
+        verses_dict = defaultdict(dict)
+
+        reg_noteref = r'([a-zA-Z\u00C0-\u00FF]+)(\[(\d+)\])'
+        reg_stage_direction = r'\((.*?)\)'
+
+        cpt = 0
+
+        for verse in verses:
+            #print(cpt, verse)
+            match = re.match(reg_stage_direction, verse)
+
+            if match:
+                text = '<% {text} | sd %>'.format(text=match.group(1))
+                verse_res = dict(text=text)
+            else:
+                self.verse_id += 1
+                text = re.sub(reg_noteref, r'<% \1 | note_\3 %>', verse)
+                verse_res = dict(id=self.verse_id, text=text)
+
+            verses_dict[cpt] = verse_res
+            cpt += 1
+
+        return dict(verses_dict)
+
+    def analyse_act(self):
+
+        temp = defaultdict(dict)
+
+        for scene in range(1, self.nb_scene-2):
+
+            print(scene, self.get_scene_characters(scene))
+            temp[scene] = dict(
+                characters=self.get_scene_characters(scene),
+                dialogues=self.scene_lines_to_struct(scene)
+            )
+
+        self.act_dict[self.act] = dict(
+            scenes=temp
+        )
+
+    def save_json(self, filename):
+        #'test.json'
+        with io.open(filename, 'w', encoding='utf8') as outfile:
+            json.dump(dict(self.act_dict), outfile, ensure_ascii=False, indent=2)
--- main.py
+++ main.py
@@ -2,7 +2,53 @@ import os
 from livrel import Theatre
 
 filepath = os.path.join('data', 'tartuffe_1.txt')
+#filepath = os.path.join('data', 'test.txt')
 
 theatre = Theatre.Theatre(filepath, 1)
 
-print(theatre.characters)
+#print(theatre.nb_scene)
+#print(theatre.characters)
+
+#theatre.scene_lines_to_struct(1)
+
+verses = [
+    'Voilà les contes bleus qu’il vous faut pour vous plaire,',
+    'Ma bru. L’on est chez vous contrainte de se taire :',
+    'Car madame, à jaser, tient le dé tout le jour.',
+    'Mais enfin je prétends discourir à mon tour :',
+    'Je vous dis que mon fils n’a rien fait de plus sage',
+    'Qu’en recueillant chez soi ce dévot personnage ;',
+    'Que le ciel au besoin l’a céans envoyé',
+    'Pour redresser à tous votre esprit fourvoyé ;',
+    'Que, pour votre salut, vous le devez entendre,',
+    'Et qu’il ne reprend rien qui ne soit à reprendre.',
+    'Ces visites, ces bals, ces conversations,',
+    'Sont du malin esprit toutes inventions.',
+    'Là, jamais on n’entend de pieuses paroles ;',
+    'Ce sont propos oisifs, chansons, et fariboles :',
+    'Bien souvent le prochain en a sa bonne part,',
+    'Et l’on y sait médire et du tiers et du quart.',
+    'Enfin les gens sensés ont leurs têtes troublées',
+    'De la confusion de telles assemblées :',
+    'Mille caquets divers s’y font en moins de rien ;',
+    'Et, comme l’autre jour un docteur dit fort bien,',
+    'C’est véritablement la tour de Babylone[9],',
+    'Car chacun y babille, et tout du long de l’aune[10] ;',
+    'Et, pour conter l’histoire où ce point l’engagea…',
+    '(Montrant Cléante.)',
+    'Voilà-t-il pas monsieur qui ricane déjà !',
+    'Allez chercher vos fous qui vous donnent à rire,',
+    '(À Elmire.)',
+    'Et sans… Adieu, ma bru ; je ne veux plus rien dire.',
+    'Sachez que pour céans j’en rabats de moitié,',
+    'Et qu’il fera beau temps quand j’y mettrai le pied.',
+    '(Donnant un soufflet à Flipote.)',
+    'Allons, vous, vous rêvez et bayez aux corneilles.',
+    'Jour de Dieu ! je saurai vous frotter les oreilles.',
+    'Marchons, gaupe, marchons[11].'
+]
+print('------------------------------')
+#theatre.verses_to_struct(verses)
+
+
+#theatre.analyse_act()
